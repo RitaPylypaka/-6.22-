e8aa05d71c215937e3e9cc6b2f90d587c7134e8d
RitaPylypaka <ritapylypaka@gmail.com>
2023-11-01 16:50:41 +0200

    Add project files.

---
+++ UnitTest6.2рек/UnitTest6.2рек.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../пр6.2рек/пр6.2рек.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62рек
+{
+	TEST_CLASS(UnitTest62рек)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int m = 8;
+			int n[m] = { 1,2,3,4,5,6,7,8 };
+			double d = Find(n, m,0,0,0);
+			Assert::AreEqual(4, d, 0.1);
+		}
+	};
+}
---
+++ пр6.2рек/пр6.2рек.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream> 
+#include <time.h>
+#include <iomanip>
+using namespace std;
+
+
+void Create(int* x, const int size, const int Low, const int High, int i)
+{
+    x[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(x, size, Low, High, i + 1);
+}
+void Print(int* x, const int size, int i)
+{
+    cout << setw(4) << x[i];
+    if (i < size - 1)
+        Print(x, size, i + 1);
+    else
+        cout << endl;
+}
+double Find(int* a, int size, int i, int p, int y) {
+    if (i == size) {
+        if (y > 0)
+            return (p * 1.0) / (y * 1.0);
+        else
+            return -1;
+    }
+
+    if (a[i] % 2 == 0) {
+        p = p + i;
+        y++;
+    }
+
+    return Find(a, size, i + 1, p, y);
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    const int n = 5;
+    int a[n];
+    int Low = 8;
+    int High = 18;
+    Create(a, n, Low, High,0);
+    Print(a, n,0);
+    cout << Find(a, n,0,0,0) << endl;
+    return 0;
+}
